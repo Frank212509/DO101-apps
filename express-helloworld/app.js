2f22128cd24702df04d95f6049463a6e0a5bb800
Frank212509 <f.alexander.t@gmail.com>
2020-07-23 09:57:49 -0500

    updated app to v2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,19 +1,26 @@
 var express = require('express');
 app = express();
 
+//app.get('/', function (req, res) {
+//  res.send('Hello World!\n');
+//});
+
+var response;
+
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
-});
+  response = 'This is version 2 of the app.' + '\n';
 
-app.get('/frank', function (req, res) {
-  res.send('Hello Frank! esto es una prueba\n');
-});
+  //send the response to the client
+  res.send(response);
 
-pp.get('/torres', function (req, res) {
-  res.send('Hello Torres! esto es una prueba\n');
-});
+//app.get('/frank', function (req, res) {
+//  res.send('Hello Frank! esto es una prueba\n');
+//});
+
+//pp.get('/torres', function (req, res) {
+//  res.send('Hello Torres! esto es una prueba\n');
+//});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
-
